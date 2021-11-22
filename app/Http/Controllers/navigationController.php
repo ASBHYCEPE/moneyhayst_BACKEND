3f436972398d7f7925764423da64625d5e8a52a8
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-11-22 21:01:14 +0800

    Added code for computing total income, expense and balance

--- app/Http/Controllers/navigationController.php
+++ app/Http/Controllers/navigationController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class navigationController extends Controller
 {
     public function main(){
-        return view('index');
+
+        $totalIncome = DB::table('transactions')->where('transac_type', 0)->sum('amount');
+        $totalExpense = DB::table('transactions')->where('transac_type', 1)->sum('amount');
+        $totalBalance = $totalIncome - $totalExpense;
+
+        return view('index',['totalIncome' => $totalIncome, 
+                             'totalExpense' => $totalExpense,
+                             'totalBalance' => $totalBalance
+                            ]);
     }
 
     public function income(){
